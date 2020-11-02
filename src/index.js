a8c7b1450e2ca70fea9d8111dd4e52b1b6872119
Wen Jen <wjen1214@gmail.com>
2020-11-01 17:14:41 -0800

    add column and rows placement

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
       }],
       stepNumber: 0,
       xIsNext: true
@@ -62,6 +62,17 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
+    const locations = [
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [1, 2],
+      [2, 2],
+      [3, 2],
+      [1, 3],
+      [2, 3],
+      [3, 3]
+    ];
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -71,7 +82,8 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares
+        squares: squares,
+        locations: locations[i],
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -91,7 +103,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + ' @ ' + history[move].locations :
         'Go to game start';
       return (
         <li key={move}>
